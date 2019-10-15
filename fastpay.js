5dd0d607282b6ba6c06238bc6530b05018cbe458
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2019-10-15 13:23:31 +0100

    add edits

--- fastpay.js
+++ fastpay.js
@@ -17,7 +17,7 @@
    * server origin
    */
    // https://fast-pay-server.herokuapp.com
-  var origin = "http://localhost:8050";
+  var origin = "https://fast-pay-server.herokuapp.com";
   /**
    * path to Fast Pay Form source
    */
